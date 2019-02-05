4ff9f61d573d21acaab8202afaa855759da219e5
github username <github email address>
2019-02-05 18:57:37 -0500

    test

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,23 @@
 class PropertiesController < ApplicationController
+
+  def new
+  end
+
+  def index
+    @properties = Property.all
+  end
+
+  def show
+    @property = Property.find(params[:id])
+  end
+
+  def edit
+    @property = Property.find(params[:id])
+  end
+
+  def update
+    @property = Property.find(params[:id])
+    @property.update(address: params[:address], price: params[:price], bedrooms: params[:bedrooms], bedrooms: params[:bathrooms] )
+    redirect_to property_path(@property)
+  end
 end
