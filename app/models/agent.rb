740acdf3c8b92312f64bd992ba8fcdd6322e2e51
github username <github email address>
2019-02-05 15:03:14 -0500

    migrations

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,2 +1,3 @@
 class Agent < ApplicationRecord
+  has_secure_password
 end
--- db/migrate/20190205173744_create_agents.rb
+++ db/migrate/20190205173744_create_agents.rb
@@ -2,6 +2,7 @@ class CreateAgents < ActiveRecord::Migration[5.2]
   def change
     create_table :agents do |t|
       t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
