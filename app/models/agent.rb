93d3fc89b6c44012c4e453bf81903cc3eef589c4
github username <github email address>
2019-02-08 10:28:54 -0500

    added has_many/belongs to

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,3 +1,5 @@
 class Agent < ApplicationRecord
   has_secure_password
+  has_many :properties
+  has_many :clients, through: :properties
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,4 @@
 class Client < ApplicationRecord
-
+  has_many :properties
+  has_many :agents, through: :properties
 end
