cdfdd112737eaa16df36dd350e36ed0de5c71010
github username <github email address>
2019-02-12 10:47:35 -0500

    added some validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,5 +3,8 @@ class Property < ApplicationRecord
   belongs_to :client, optional: true
 
   validates :address, presence: true
-
+  validates :address, uniqueness: true
+  validates :price, presence: true
+  validates :bathrooms, presence: true, numericality: { greater_than: 0 }
+  validates :bedrooms, presence: true, numericality: { greater_than: 0 }
 end
