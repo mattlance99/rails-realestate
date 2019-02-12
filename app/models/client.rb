5624533154927d01d748b911a218f096819cb7cd
github username <github email address>
2019-02-12 14:28:32 -0500

    added client forms

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,9 @@
 class Client < ApplicationRecord
   has_many :properties
   has_many :agents, through: :properties
+
+  def last_name_first_name
+    self.last_name + ", " + self.first_name
+  end
+
 end
